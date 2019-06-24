88af07fcf130a167c61f706db09a2e80018eaa17
vincentni-jrvs <vincent.ni.jrvs.ca@gmail.com>
2019-06-24 18:26:11 +0000

    rename and relocate files to meet submission requirement

---
+++ scripts/host_info_old.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##Store input for later use.
+hostName=$1
+port=$2
+dbname=$3
+user=$4
+pw=$5
+
+##Gather information about the CPU and Total Memory
+myid=$(PGPASSWORD=$pw psql -U $user -h $hostName -d $dbname -c "select count(*)+1 from host_info" |  tail -n3 | head -1 )
+echo $myid > id.txt
+hostname=$(hostname -f)
+cpu_model=$(lscpu | grep 'Model name' | awk -F': *' '{print $2}')
+cpu_number=$(lscpu | grep -m 1 'CPU(s)' | awk -F': *' '{print $2}')
+cpu_architecture=$(lscpu | grep -m 1 'Architecture' | awk -F': *' '{print $2}')
+cpu_mhz=$(lscpu | grep -m 1 'CPU MHz' | awk -F': *' '{print $2}')
+l2_cache=$(lscpu | grep -m 1 'L2 cache' | awk -F': *' '{print $2}'| sed 's/[^0-9]*//g')
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+total_mem=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
+
+##Push the Insert statement to PSQL
+PGPASSWORD=$pw psql -U $user -h $hostName -d $dbname -c "insert into host_info
+values('$myid','$hostname','$cpu_number','$cpu_architecture','$cpu_model','$cpu_mhz','$l2_cache','$timestamp','$total_mem')"
--- scripts/host_usage.sh
+++ scripts/host_usage.sh
@@ -1,20 +1,61 @@
-#!/bin/bash
-##Local variable
-hostName=$1
+#! /bin/bash
+
+#Setup arguments
+psql_host=$1
 port=$2
-dbname=$3
-user=$4
-pw=$5
+db_name=$3
+user_name=$4
+password=$5
 
-#Gather information about memory usage and cpu usage
+
+get_host_id() {
+	hostid=$(cat ~/host_id)
+}
+
+#Helper function
+get_vmstat_value() {
+	value=$(vmstat $1| tail -n1 | awk -v temp=$2 '{print $temp}' | xargs)
+	#echo "value=$value"
+}
+
+get_memory_free() {
+	get_vmstat_value "" "4"
+	memory_free=$value
+}
+
+get_cpu_idle() {
+	get_vmstat_value "" "15"
+	cpu_idle=$value
+}
+
+get_cpu_kernel() {
+	get_vmstat_value "" "14"
+	cpu_kernel=$value
+}
+
+get_disk_io() {
+	get_vmstat_value "-d" "10"
+	disk_io=$value
+}
+
+
+#Step 1: parse data and setup variables
+get_host_id
+get_memory_free
+get_cpu_idle
+get_cpu_kernel
+get_disk_io
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-hostid=$(cat $PWD/id.txt)
-memory_free=$(vmstat | tail -n1 | awk '{print $4}')
-cpu_idle=$(vmstat | tail -n1 | awk '{print $15}')
-cpu_kernel=$(vmstat | tail -n1 | awk '{print $14}')
-disk_io=$(vmstat -d | tail -n1 | awk '{print $10}')
 disk_available=$(df -BM --output=avail "$PWD" | tail -n1 | sed 's/[^0-9]*//g')
-timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
-##Push insert statement to psql
-PGPASSWORD=$pw psql -U $user -h $hostName -d $dbname -c "insert into host_usage values ('$timestamp','$hostid','$memory_free',$cpu_idle,'$cpu_kernel','$disk_io','$disk_available');"
+#Step 2: construct INSERT statement
+insert_stmt=$(cat <<-END
+INSERT INTO host_usage VALUES('${timestamp}','${hostid}',${memory_free},${cpu_idle},${cpu_kernel},${disk_io},${disk_available});
+END
+)
+#echo $insert_stmt
+
+#Step 3: Execute INSERT statement
+export PGPASSWORD=$password
+psql -h $psql_host -p $port -U $user_name -d $db_name -c "$insert_stmt"
+sleep 1
